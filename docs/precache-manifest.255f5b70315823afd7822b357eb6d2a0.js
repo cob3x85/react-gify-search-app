f55261aef70777066af59997d766a768689f85b3
cob3x85 <cob3x85@outlook.com>
2020-07-19 23:04:04 -0500

    Rename build folder to docs to publish in github pages

---
+++ docs/precache-manifest.255f5b70315823afd7822b357eb6d2a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35ed97d7b4faaf52f8ce3bb79aa592e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed19c6687081f185bc58",
+    "url": "/static/css/main.80df7821.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed19c6687081f185bc58",
+    "url": "/static/js/main.c2484357.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
